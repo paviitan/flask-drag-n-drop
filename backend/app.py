f4287364508aee3ddc66443988abd00dd1cbfde1
Pasi Viitanen <pasi.viitanen@huld.io>
2022-10-20 13:44:44 +0300

    Move frontend stuff to Flask render templates
    Now I am a little bit confused. Looks like I don't have to use nodes http-server and flask together.
    Just using flask is fine?

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
+from flask import render_template
 
 app = Flask(__name__)
 CORS(app) # CORS allows frontend JavaScript with Python backend
 
 @app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+    
+@app.route("/lobby", methods=["GET"])    
 def lobby():
     return "<p>You arrive at the hotel lobby. A receptionist waits in silence...</p>"
+    
+@app.route("/exit/", methods=["GET"])
+def exit():
+    return render_template("exit.html")
     
 @app.route("/greet", methods=["GET"])
 def greet():
